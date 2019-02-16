f1eb0d6959f99919af6666799a656b4df89bc748
Luke <lukekennedy212@gmail.com>
2019-02-16 11:37:53 +0800

    added the oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,29 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :authentications, dependent: :destroy
   
   validates :email, presence: true, uniqueness: true 
+
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+    user = User.find_by(email: auth_hash["info"]["email"])
+    if !user
+      # byebug
+      user = self.create!(
+      first_name: auth_hash["info"]["first_name"],
+      last_name: auth_hash["info"]["last_name"],
+      email: auth_hash["info"]["email"],
+      password: SecureRandom.hex(10)
+      )
+      user.save
+    end
+    user.authentications << authentication
+    return user
+  end
+
+  # grab google to access google for user data
+  def google_token
+    x = self.authentications.find_by(provider: 'google_oauth2')
+    return x.token unless x.nil?
+  end
 end
