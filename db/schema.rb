0c0ec507e71ca900c8aba9cefdf863d5274718a7
Luke <lukekennedy212@gmail.com>
2019-02-19 19:43:00 +0800

    added the attendee and the add event if never hosted event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_101554) do
+ActiveRecord::Schema.define(version: 2019_02_19_111930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_02_19_101554) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
-    t.boolean "added_to_google_calendar", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
