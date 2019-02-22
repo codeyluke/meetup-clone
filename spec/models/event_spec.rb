d0b6e39eebac4acacc71e6f438da96f7251ebfd3
Luke <lukekennedy212@gmail.com>
2019-02-22 15:21:30 +0800

    fix the custom method error

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -68,28 +68,42 @@ RSpec.describe Event, type: :model do
         ########################################### HAPPY ###########################################
         
         let (:params_1) {{event_title: "title_1", location: "location_1", description: "describe_1", start_time: Time.now, end_time: Time.now}}
-        let (:params_2) {{event_title: "title_2", location: "location_2", description: "describe_2", start_time: Time.now, end_time: Time.now}} 
+        let (:params_2) {{event_title: "title_2", location: "location_2", description: "describe_2", start_time: Time.now + 1, end_time: Time.now + 1}} 
         
         before :each do 
             ActiveRecord::Base.connection.reset_pk_sequence!(:events) 
         end
 
-        it "#search_event_title(search) && #search_event_description(search) && search_event_date(search)" do 
+        it "#search_event_title(search)" do 
             event_1 = Event.new(params_1).save
             event_2 = Event.new(params_2).save
             events = Event.all
-            event = Event.where("event_title ILIKE ? ", "title_1")
+            event = Event.search_event_title("title_1")
+            expect(events.length).to_not eq(event.length)
+        end
+
+        it "#search_event_description(search)" do 
+            event_1 = Event.new(params_1).save
+            event_2 = Event.new(params_2).save
+            events = Event.all
+            event = Event.search_event_description("describe_1")
             expect(events.length).to_not eq(event.length)
         end
 
         ########################################### EDGY ###########################################
 
-        it "EDGY #search_event_title(search) && #search_event_description(search) && search_event_date(search)" do 
+        it "EDGY #search_event_title(search)" do 
             event_1 = Event.new(params_1).save
             event_2 = Event.new(params_2).save
             events = Event.all
-            event = Event.where("event_title ILIKE ? ", "title_1")
-            expect(events.length).to eq(events.length)
+            expect{Event.search_event_title}.to raise_error(ArgumentError)
+        end
+
+        it "EDGY #search_event_description(search)" do 
+            event_1 = Event.new(params_1).save
+            event_2 = Event.new(params_2).save
+            events = Event.all
+            expect{Event.search_event_description}.to raise_error(ArgumentError)
         end
     end
 end
