7ef29251aef49eb650b211d383a1292dd74940cd
Luke <lukekennedy212@gmail.com>
2019-02-25 16:41:14 +0800

    fixed omni

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
-    OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+    OmniAuth.config.full_host = Rails.env.production? ? 'https://next-meetup-clone.herokuapp.com/' : 'http://localhost:3000'
 end
